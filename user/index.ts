972debf796d8be151cb9091b71e76167b4db2edf
Alexander James Malcolm <alexjamesmalcolm@gmail.com>
2020-04-17 16:02:17 -0400

    Tried to log the document better

--- user/index.ts
+++ user/index.ts
@@ -2,6 +2,7 @@ import {
   APIGatewayProxyEvent,
   APIGatewayProxyResult,
   Context,
+  Doc,
 } from "../deps.ts";
 import { log } from "../utils.ts";
 import { client } from "../client.ts";
@@ -10,7 +11,9 @@ export async function userHandler(
   event: APIGatewayProxyEvent,
   context: Context,
 ): Promise<APIGatewayProxyResult> {
-  return log(client.listTables());
+  const document: Doc = await client.listTables();
+  const result = { document: JSON.stringify(document) };
+  return log(result);
   // return {
   //   statusCode: 200,
   //   headers: { "content-type": "text/html;charset=utf8" },
