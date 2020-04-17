20d779eae6a0335029a36a193ce01d798989705e
Alexander James Malcolm <alexjamesmalcolm@gmail.com>
2020-04-17 15:51:04 -0400

    Logging list of tables

--- user/index.ts
+++ user/index.ts
@@ -4,12 +4,13 @@ import {
   Context,
 } from "../deps.ts";
 import { log } from "../utils.ts";
+import { client } from "../client.ts";
 
 export async function userHandler(
   event: APIGatewayProxyEvent,
   context: Context,
 ): Promise<APIGatewayProxyResult> {
-  return log(event);
+  return log(client.listTables());
   // return {
   //   statusCode: 200,
   //   headers: { "content-type": "text/html;charset=utf8" },
