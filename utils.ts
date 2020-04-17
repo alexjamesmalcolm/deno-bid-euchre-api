bfb6363ecbd929ef4d74b2fb91ebceb563b5f289
Alexander James Malcolm <alexjamesmalcolm@gmail.com>
2020-04-17 16:30:51 -0400

    created badMethod response

--- utils.ts
+++ utils.ts
@@ -1,5 +1,14 @@
+import { APIGatewayProxyResult } from "./deps.ts";
+
 export const log = (a: any) => ({
   statusCode: 200,
   headers: { "content-type": "application/json" },
   body: JSON.stringify(a, null, 2),
+});
+
+export const badMethod = (
+  method: string,
+): APIGatewayProxyResult => ({
+  statusCode: 405,
+  body: `The ${method} is not supported`,
 });
